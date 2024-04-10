cfdae197505e9881f2b916c91dcc2e1ef7da6467
hendlegleg <hendlegleg1@gmail.com>
2024-04-10 01:13:20 +0100

    3commit

---
+++ lib/models/restaurant.dart
@@ -0,0 +1,319 @@
+import 'food.dart';
+
+class Restaurant {
+  final List<Food> _menu = [
+    Food(
+      name: "classic cheese bruger",
+      description:
+          "C'est un sandwich composé d'un steak haché, de fromage fondu entre deux pains à hamburger.",
+      imagePath: "lib/images/burgers/a.jpg",
+      price: 15,
+      category: FoodCategory.hamburgers,
+      availableAddons: [
+        Addon(name: "Supplément fromage", price: 3),
+        Addon(name: "Bacon", price: 4),
+        Addon(name: "Avocat", price: 5),
+      ],
+    ),
+    Food(
+      name: "BBQ Bacon Burger",
+      description:
+          "C'est un sandwich composé d'un steak haché, de fromage fondu entre deux pains à hamburger.",
+      imagePath: "lib/images/burgers/a.jpg",
+      price: 15,
+      category: FoodCategory.hamburgers,
+      availableAddons: [
+        Addon(name: "Supplément fromage", price: 3),
+        Addon(name: "Bacon", price: 4),
+        Addon(name: "Avocat", price: 5),
+      ],
+    ),
+    Food(
+      name: "Veggie Burger",
+      description:
+          "C'est un sandwich composé d'un steak haché, de fromage fondu entre deux pains à hamburger.",
+      imagePath: "lib/images/burgers/a.jpg",
+      price: 15,
+      category: FoodCategory.hamburgers,
+      availableAddons: [
+        Addon(name: "Supplément fromage", price: 3),
+        Addon(name: "Bacon", price: 4),
+        Addon(name: "Avocat", price: 5),
+      ],
+    ),
+    Food(
+      name: "Aloha Burger",
+      description:
+          "C'est un sandwich composé d'un steak haché, de fromage fondu entre deux pains à hamburger.",
+      imagePath: "lib/images/burgers/a.jpg",
+      price: 15,
+      category: FoodCategory.hamburgers,
+      availableAddons: [
+        Addon(name: "Supplément fromage", price: 3),
+        Addon(name: "Bacon", price: 4),
+        Addon(name: "Avocat", price: 5),
+      ],
+    ),
+    Food(
+      name: "Blue Moon Burger",
+      description:
+          "C'est un sandwich composé d'un steak haché, de fromage fondu entre deux pains à hamburger.",
+      imagePath: "lib/images/burgers/a.jpg",
+      price: 15,
+      category: FoodCategory.hamburgers,
+      availableAddons: [
+        Addon(name: "Supplément fromage", price: 3),
+        Addon(name: "Bacon", price: 4),
+        Addon(name: "Avocat", price: 5),
+      ],
+    ),
+    Food(
+      name: "Salade César",
+      description:
+          "C'est un sandwich composé d'un steak haché, de fromage fondu entre deux pains à hamburger.",
+      imagePath: "lib/images/burgers/a.jpg",
+      price: 15,
+      category: FoodCategory.hamburgers,
+      availableAddons: [
+        Addon(name: "Supplément fromage", price: 3),
+        Addon(name: "Bacon", price: 4),
+        Addon(name: "Avocat", price: 5),
+      ],
+    ),
+    Food(
+      name: "Salade Grècque",
+      description:
+          "C'est un sandwich composé d'un steak haché, de fromage fondu entre deux pains à hamburger.",
+      imagePath: "lib/images/burgers/a.jpg",
+      price: 15,
+      category: FoodCategory.hamburgers,
+      availableAddons: [
+        Addon(name: "Supplément fromage", price: 3),
+        Addon(name: "Bacon", price: 4),
+        Addon(name: "Avocat", price: 5),
+      ],
+    ),
+    Food(
+      name: "Salade de Quinoa",
+      description:
+          "C'est un sandwich composé d'un steak haché, de fromage fondu entre deux pains à hamburger.",
+      imagePath: "lib/images/burgers/a.jpg",
+      price: 15,
+      category: FoodCategory.hamburgers,
+      availableAddons: [
+        Addon(name: "Supplément fromage", price: 3),
+        Addon(name: "Bacon", price: 4),
+        Addon(name: "Avocat", price: 5),
+      ],
+    ),
+    Food(
+      name: "Salade Asiatique au Sésame",
+      description:
+          "C'est un sandwich composé d'un steak haché, de fromage fondu entre deux pains à hamburger.",
+      imagePath: "lib/images/burgers/a.jpg",
+      price: 15,
+      category: FoodCategory.hamburgers,
+      availableAddons: [
+        Addon(name: "Supplément fromage", price: 3),
+        Addon(name: "Bacon", price: 4),
+        Addon(name: "Avocat", price: 5),
+      ],
+    ),
+    Food(
+      name: "Salade de Poulet du Sud-Ouest",
+      description:
+          "C'est un sandwich composé d'un steak haché, de fromage fondu entre deux pains à hamburger.",
+      imagePath: "lib/images/burgers/a.jpg",
+      price: 15,
+      category: FoodCategory.hamburgers,
+      availableAddons: [
+        Addon(name: "Supplément fromage", price: 3),
+        Addon(name: "Bacon", price: 4),
+        Addon(name: "Avocat", price: 5),
+      ],
+    ),
+    Food(
+      name: "Frites de patates douces croustillantes",
+      description:
+          "C'est un sandwich composé d'un steak haché, de fromage fondu entre deux pains à hamburger.",
+      imagePath: "lib/images/burgers/a.jpg",
+      price: 15,
+      category: FoodCategory.hamburgers,
+      availableAddons: [
+        Addon(name: "Supplément fromage", price: 3),
+        Addon(name: "Bacon", price: 4),
+        Addon(name: "Avocat", price: 5),
+      ],
+    ),
+    Food(
+      name: "Onion Rings",
+      description:
+          "C'est un sandwich composé d'un steak haché, de fromage fondu entre deux pains à hamburger.",
+      imagePath: "lib/images/burgers/a.jpg",
+      price: 15,
+      category: FoodCategory.hamburgers,
+      availableAddons: [
+        Addon(name: "Supplément fromage", price: 3),
+        Addon(name: "Bacon", price: 4),
+        Addon(name: "Avocat", price: 5),
+      ],
+    ),
+    Food(
+      name: "Pain à l'Ail",
+      description:
+          "C'est un sandwich composé d'un steak haché, de fromage fondu entre deux pains à hamburger.",
+      imagePath: "lib/images/burgers/a.jpg",
+      price: 15,
+      category: FoodCategory.hamburgers,
+      availableAddons: [
+        Addon(name: "Supplément fromage", price: 3),
+        Addon(name: "Bacon", price: 4),
+        Addon(name: "Avocat", price: 5),
+      ],
+    ),
+    Food(
+      name: "Frites de patates douces garnies",
+      description:
+          "C'est un sandwich composé d'un steak haché, de fromage fondu entre deux pains à hamburger.",
+      imagePath: "lib/images/burgers/a.jpg",
+      price: 15,
+      category: FoodCategory.hamburgers,
+      availableAddons: [
+        Addon(name: "Supplément fromage", price: 3),
+        Addon(name: "Bacon", price: 4),
+        Addon(name: "Avocat", price: 5),
+      ],
+    ),
+    Food(
+      name: "Bouchées croustillantes de macaroni au fromage",
+      description:
+          "C'est un sandwich composé d'un steak haché, de fromage fondu entre deux pains à hamburger.",
+      imagePath: "lib/images/burgers/a.jpg",
+      price: 15,
+      category: FoodCategory.hamburgers,
+      availableAddons: [
+        Addon(name: "Supplément fromage", price: 3),
+        Addon(name: "Bacon", price: 4),
+        Addon(name: "Avocat", price: 5),
+      ],
+    ),
+    Food(
+      name: "Brownie au chocolat",
+      description:
+          "C'est un sandwich composé d'un steak haché, de fromage fondu entre deux pains à hamburger.",
+      imagePath: "lib/images/burgers/a.jpg",
+      price: 15,
+      category: FoodCategory.hamburgers,
+      availableAddons: [
+        Addon(name: "Supplément fromage", price: 3),
+        Addon(name: "Bacon", price: 4),
+        Addon(name: "Avocat", price: 5),
+      ],
+    ),
+    Food(
+      name: "Cheesecake",
+      description:
+          "C'est un sandwich composé d'un steak haché, de fromage fondu entre deux pains à hamburger.",
+      imagePath: "lib/images/burgers/a.jpg",
+      price: 15,
+      category: FoodCategory.hamburgers,
+      availableAddons: [
+        Addon(name: "Supplément fromage", price: 3),
+        Addon(name: "Bacon", price: 4),
+        Addon(name: "Avocat", price: 5),
+      ],
+    ),
+    Food(
+      name: "Tarte aux pommes",
+      description:
+          "C'est un sandwich composé d'un steak haché, de fromage fondu entre deux pains à hamburger.",
+      imagePath: "lib/images/burgers/a.jpg",
+      price: 15,
+      category: FoodCategory.hamburgers,
+      availableAddons: [
+        Addon(name: "Supplément fromage", price: 3),
+        Addon(name: "Bacon", price: 4),
+        Addon(name: "Avocat", price: 5),
+      ],
+    ),
+    Food(
+      name: "Red Velvet",
+      description:
+          "C'est un sandwich composé d'un steak haché, de fromage fondu entre deux pains à hamburger.",
+      imagePath: "lib/images/burgers/a.jpg",
+      price: 15,
+      category: FoodCategory.hamburgers,
+      availableAddons: [
+        Addon(name: "Supplément fromage", price: 3),
+        Addon(name: "Bacon", price: 4),
+        Addon(name: "Avocat", price: 5),
+      ],
+    ),
+    Food(
+      name: "Lemonade",
+      description:
+          "C'est un sandwich composé d'un steak haché, de fromage fondu entre deux pains à hamburger.",
+      imagePath: "lib/images/burgers/a.jpg",
+      price: 15,
+      category: FoodCategory.hamburgers,
+      availableAddons: [
+        Addon(name: "Supplément fromage", price: 3),
+        Addon(name: "Bacon", price: 4),
+        Addon(name: "Avocat", price: 5),
+      ],
+    ),
+    Food(
+      name: "Iced Tea",
+      description:
+          "C'est un sandwich composé d'un steak haché, de fromage fondu entre deux pains à hamburger.",
+      imagePath: "lib/images/burgers/a.jpg",
+      price: 15,
+      category: FoodCategory.hamburgers,
+      availableAddons: [
+        Addon(name: "Supplément fromage", price: 3),
+        Addon(name: "Bacon", price: 4),
+        Addon(name: "Avocat", price: 5),
+      ],
+    ),
+    Food(
+      name: "Smoothie",
+      description:
+          "C'est un sandwich composé d'un steak haché, de fromage fondu entre deux pains à hamburger.",
+      imagePath: "lib/images/burgers/a.jpg",
+      price: 15,
+      category: FoodCategory.hamburgers,
+      availableAddons: [
+        Addon(name: "Supplément fromage", price: 3),
+        Addon(name: "Bacon", price: 4),
+        Addon(name: "Avocat", price: 5),
+      ],
+    ),
+    Food(
+      name: "Mojito",
+      description:
+          "C'est un sandwich composé d'un steak haché, de fromage fondu entre deux pains à hamburger.",
+      imagePath: "lib/images/burgers/a.jpg",
+      price: 15,
+      category: FoodCategory.hamburgers,
+      availableAddons: [
+        Addon(name: "Supplément fromage", price: 3),
+        Addon(name: "Bacon", price: 4),
+        Addon(name: "Avocat", price: 5),
+      ],
+    ),
+    Food(
+      name: "Caramel Macchiato",
+      description:
+          "C'est un sandwich composé d'un steak haché, de fromage fondu entre deux pains à hamburger.",
+      imagePath: "lib/images/burgers/a.jpg",
+      price: 15,
+      category: FoodCategory.hamburgers,
+      availableAddons: [
+        Addon(name: "Supplément fromage", price: 3),
+        Addon(name: "Bacon", price: 4),
+        Addon(name: "Avocat", price: 5),
+      ],
+    ),
+  ];
+  List<Food> get menu => _menu;
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter_application_1/components/my_current_location.dart';
 import 'package:flutter_application_1/components/my_description_box.dart';
 import 'package:flutter_application_1/components/my_drawer.dart';
 import 'package:flutter_application_1/components/my_silver_app_bar.dart';
+import 'package:flutter_application_1/components/my_tab_bar.dart';
+import 'package:flutter_application_1/models/food.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,7 +13,22 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  @override
+  void initState() {
+    super.initState();
+    _tabController =
+        TabController(length: FoodCategory.values.length, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +36,9 @@ class _HomePageState extends State<HomePage> {
       body: NestedScrollView(
         headerSliverBuilder: (context, innerBoxIsScrolled) => [
           MySilverAppBar(
-            title: Text('title'),
+            title: MyTabBar(
+              tabController: _tabController,
+            ),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
@@ -34,7 +53,31 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
         ],
-        body: Container(color: Colors.blue),
+        body: TabBarView(
+          controller: _tabController,
+          children: [
+            ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) => Text("first tab items"),
+            ),
+            ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) => Text("second tab items"),
+            ),
+            ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) => Text("third tab items"),
+            ),
+            ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) => Text("third tab items"),
+            ),
+            ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) => Text("third tab items"),
+            ),
+          ],
+        ),
       ),
     );
   }
